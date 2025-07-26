6c9d8ad3555b8b27692c2bf0e1aa61bbf1916eb1
Ale <avilalejandram@gmail.com>
2025-07-25 21:22:51 -0600

    2

--- Actividad10.py
+++ Actividad10.py
@@ -1,4 +1,46 @@
+def invertir_texto(palabra):
+    if palabra=="":
+        return ""
+    else:
+        return invertir_texto(palabra[1:]) + palabra[0]
+def primeros_naturales (n):
+    if n==0:
+        return 0
+    else:
+        return n+primeros_naturales(n-1)
+def cuenta_regresiva (n):
+    if n==0:
+        return 0
+    else:
+        return cuenta_regresiva (n-1)
+
+def sumar_digitos(n):
+    if 0<n<=9:
+        return n
+    else:
+        return n%10+sumar_digitos(n//10)
+
+def contar_digitos(n):
+    if 0<n<=9:
+        return 1
+    else:
+        return 1+contar_digitos(n//10)
+
 def menu ():
     print("___Menu___")
     print ("1. Invertir cadena de texto")
     print ("2. Calcular los primeros N numeros naturales")
+    print ("3. Imprimir cuenta regresiva")
+    print ("4. Sumar los digitos de un numero")
+    print ("5. Contar cuantos digitos tiene un numero")
+    print ("6. Salir")
+    opcion=int(input("Seleccione una opcion"))
+    if opcion==1:
+        palabra=input("Ingrese una cadena de texto: ")
+        print(f"La palabra {palabra} invertida es {invertir_texto(palabra)}")
+    elif opcion==2:
+        n=int(input("Ingrese un numero: "))
+        print(f"La suma de los primeros {n} numeros naturales es {sumar_digitos(n)}")
+    elif opcion==3:
+        n=int(input("Ingrese un numero: "))
+        print(f"{cuenta_regresiva(n)}")
